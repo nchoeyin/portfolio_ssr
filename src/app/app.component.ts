3ba661815123af928c2312edf0fe19d7ad9805bc
Ngawang Choeying <ngawangchoeying303@gmail.com>
2025-06-10 20:51:42 +0530

    afternextrender() impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { afterNextRender, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpCacheComponent } from "./http-cache/http-cache.component";
 
@@ -13,6 +13,16 @@ export class AppComponent {
 
   @ViewChild('content') contentRef!: ElementRef;
 
+  constructor() {
+    afterNextRender(() => {
+      // console.log('Content reference:', this.contentRef); 
+      const contentHeight = this.contentRef.nativeElement.offsetHeight;
+      console.log('Content height:', contentHeight);
+    });
+      // This will run after the next render cycle, ensuring that the DOM is ready.
+    // console.log('AppComponent constructor called');
+    // console.log('Window object:', window);
+  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
