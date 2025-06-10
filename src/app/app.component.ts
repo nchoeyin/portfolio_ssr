cf9b56345b8d5335d1f165001b93085a3712c964
Ngawang Choeying <ngawangchoeying303@gmail.com>
2025-06-10 20:45:17 +0530

    ngOnInit() does not initialise the contentRef but

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpCacheComponent } from "./http-cache/http-cache.component";
 
@@ -10,4 +10,14 @@ import { HttpCacheComponent } from "./http-cache/http-cache.component";
 })
 export class AppComponent {
   title = 'portfolio_ssr';
+
+  @ViewChild('content') contentRef!: ElementRef;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // console.log('AppComponent initialized');
+    // console.log('Window object:', window);
+    console.log('Content reference:', this.contentRef); 
+  }
 }
